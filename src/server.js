59a44f36b921c4ed75762491a67e43b767ea33a9
JWSMartel <JWSMartel@gmail.com>
2026-01-05 03:36:38 -0500

    NodeJS Server Setup and started on Routes

--- src/server.js
+++ src/server.js
@@ -1,8 +1,19 @@
-const express = require('express');
+import express from "express";
+
+import movieRoutes from './routes/movieRoutes.js';
 
 const app = express();
+
+app.use("/movies",movieRoutes);
 
 const PORT = 5001;
 const server = app.listen(PORT,()=>{
     console.log(`Server running on PORT ${PORT}`);
 });
+
+//GET, POST, PUT, DELETE
+
+//AUTH - signin, signup
+//MOVIE - GETTING ALL MOVIES
+//USER - Profile
+//WATCHLIST
