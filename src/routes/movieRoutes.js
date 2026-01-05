0bb8e816c11a08516c74781eeb2d6bb012454cac
JWSMartel <JWSMartel@gmail.com>
2026-01-05 03:43:11 -0500

    End of Routes section

--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -2,8 +2,21 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/hello",(req, res)=>{
-    res.json({message:"hello"});
+//Front end gets data
+router.get("/",(req, res)=>{
+    res.json({httpMethod:"get"});
+});
+//Creating data
+router.post("/",(req, res)=>{
+    res.json({httpMethod:"post"});
+});
+//Updating data
+router.put("/",(req, res)=>{
+    res.json({httpMethod:"put"});
+});
+//Removing data
+router.delete("/",(req, res)=>{
+    res.json({httpMethod:"delete"});
 });
 
 export default router;
