7b943845f5497ca56edc0c5e7ad8a5f9f16d2db6
JWSMartel <JWSMartel@gmail.com>
2026-01-05 15:36:00 -0500

    Got the PUT route working for watchlist

--- src/controllers/watchlistController.js
+++ src/controllers/watchlistController.js
@@ -13,7 +13,7 @@ const addToWatchlist = async (req,res)=>{
 
     const inWatchlist = await prisma.watchlistItem.findUnique({
         where:{userId_movieId:{
-            userId:userId,
+            userId:req.user.id,
             movieId:movieId,
         },},
     });
@@ -24,7 +24,7 @@ const addToWatchlist = async (req,res)=>{
 
     const watchlistItem = await prisma.watchlistItem.create({
         data:{
-            userId,
+            userId:req.user.id,
             movieId,
             status:status||"PLANNED",
             rating,
@@ -40,4 +40,71 @@ const addToWatchlist = async (req,res)=>{
     });
 };
 
-export {addToWatchlist};
+const removeFromWatchlist = async(req, res)=>{
+    const watchlistItem = await prisma.watchlistItem.findUnique({
+        where:{id:req.params.id}
+    });
+
+    if(!watchlistItem){
+        return res.status(404).json({error:"Watchlist item not found"});
+    }
+
+    if(watchlistItem.userId!== req.user.id){
+        return res.status(403).json({error:"Not allowed to update this watchlist item"});
+    }
+
+    await prisma.watchlistItem.delete({
+        where:{id:req.params.id}
+    });
+    res.status(200).json({
+        status:"success",
+        message:"Movie removed from watchlist"
+    })
+};
+
+const updateWatchlistItem = async(req, res)=>{
+    const {status, rating, notes} = req.body;
+    console.log("STATUS:",status);
+    console.log("RATING:",rating);
+    console.log("NOTES:",notes);
+    const watchlistItem = await prisma.watchlistItem.findUnique({
+        where:{id:req.params.id}
+    });
+
+    if(!watchlistItem){
+        return res.status(404).json({error:"Watchlist item not found"});
+    }
+
+    if(watchlistItem.userId!== req.user.id){
+        return res.status(403).json({error:"Not allowed to update this watchlist item"});
+    }
+
+    const updateData = {};
+    if(status !== undefined){
+        updateData.status = status.toUpperCase();
+        console.log("Status updated");
+    }
+    if(rating!==undefined){
+        updateData.rating = rating;
+        console.log("rating updated");
+    }
+    if(notes !== undefined){
+        updateData.notes = notes;
+        console.log("notes updated");
+    }
+
+    await prisma.watchlistItem.update({
+        where:{id:req.params.id},
+        data:{
+            status:updateData.status,
+            rating:updateData.rating,
+            notes:updateData.notes,
+        }
+    });
+    res.status(200).json({
+        status:"success",
+        message:"Movie updated"
+    })
+}
+
+export {addToWatchlist, removeFromWatchlist, updateWatchlistItem};
--- src/routes/watchlistRoutes.js
+++ src/routes/watchlistRoutes.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { addToWatchlist } from "../controllers/watchlistController.js";
+import { addToWatchlist, removeFromWatchlist, updateWatchlistItem } from "../controllers/watchlistController.js";
+import {authMiddleware} from "../middleware/authMiddleware.js"
 
 const router = express.Router();
 
+router.use(authMiddleware);
+
 router.post("/", addToWatchlist);
+
+router.put("/:id", updateWatchlistItem);
+
+router.delete("/:id", removeFromWatchlist);
 
 //testing
 router.get("/",(req, res)=>{
